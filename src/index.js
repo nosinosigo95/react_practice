a2c176d09c13e4ead70fbfcbff8c18c8beb4b50a
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-08-12 10:40:15 +0900

    stateの基礎

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import EventBasic from './EventBasic';
+import StateBasic from './StateBasic';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <EventBasic type="time" />
+  <StateBasic init={0} />
 );
 
 // If you want to start measuring performance in your app, pass a function
