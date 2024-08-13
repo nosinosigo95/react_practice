23d63196ff61af53cb7d5dac069f17bcc9219442
nosinosigo95 <nosinosigo95@nosinosigo95.local>
2024-08-13 23:23:34 +0900

    form入門

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import StateParent from './StateParent';
+import StateForm from './StateForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateParent />
+  <StateForm />
 );
 
 // If you want to start measuring performance in your app, pass a function
